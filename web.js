74983810daaed425bbb23a70e4ea4689783a7ab7
eggpie76 <eggpie76@naver.com>
2026-03-13 11:08:45 +0900

    0313-07

--- web.js
+++ web.js
@@ -18,41 +18,13 @@ connection.connect((err) => {
   console.log('데이터베이스에 연결되었습니다.');
 });
 
-/*
-// 새 테이블 생성 쿼리
-const createTableQuery = `
-  CREATE TABLE goods (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    barcode VARCHAR(50),
-    name VARCHAR(255),
-    price VARCHAR(50),
-    img VARCHAR(255),
-    cdc VARCHAR(50),
-    sdcs VARCHAR(50),
-    fdcs VARCHAR(50),
-    url TEXT,
-    co VARCHAR(50),
-    cdcs VARCHAR(50)
-  )
-`;
-
-  // 새 테이블 생성
-  connection.query(createTableQuery, (err, results) => {
-    if (err) {
-      console.error('테이블 생성 실패:', err.message);
-      return;
-    }
-    console.log('새 테이블이 성공적으로 생성되었습니다.');
-    connection.end(); // 작업 완료 후 데이터베이스 연결 종료
-  });
-});
 
 // HTTP 서버 생성
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
   res.end("테이블 삭제 및 생성 작업이 완료되었습니다.");
 });
-*/
+
 
 const PORT = process.env.PORT || 8001; // Cafe24에서 제공하는 포트 사용
 server.listen(PORT, () => {
