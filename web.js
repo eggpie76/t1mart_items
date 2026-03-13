4b7cd1d7990d7b97f96084b64963a26e53e68545
eggpie76 <eggpie76@naver.com>
2026-03-13 11:07:38 +0900

    0313-06

--- web.js
+++ web.js
@@ -18,9 +18,7 @@ connection.connect((err) => {
   console.log('데이터베이스에 연결되었습니다.');
 });
 
-// 기존 테이블 삭제 쿼리
-const dropTableQuery = "DROP TABLE IF EXISTS goods";
-
+/*
 // 새 테이블 생성 쿼리
 const createTableQuery = `
   CREATE TABLE goods (
@@ -38,14 +36,6 @@ const createTableQuery = `
   )
 `;
 
-// 테이블 삭제
-//connection.query(dropTableQuery, (err, results) => {
-//  if (err) {
-//    console.error('테이블 삭제 실패:', err.message);
-//    return;
-//  }
-//  console.log('기존 테이블이 삭제되었습니다.');
-
   // 새 테이블 생성
   connection.query(createTableQuery, (err, results) => {
     if (err) {
@@ -62,6 +52,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
   res.end("테이블 삭제 및 생성 작업이 완료되었습니다.");
 });
+*/
 
 const PORT = process.env.PORT || 8001; // Cafe24에서 제공하는 포트 사용
 server.listen(PORT, () => {
