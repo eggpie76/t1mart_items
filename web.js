d54a880b5884cf8ac95b596c3474a45fc02d9a2b
eggpie76 <eggpie76@naver.com>
2026-03-13 10:38:00 +0900

    0313-01

--- web.js
+++ web.js
@@ -1,1 +1,11 @@
-abcddd
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Hello, World!" }));
+});
+
+const PORT = process.env.PORT || 8001;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
