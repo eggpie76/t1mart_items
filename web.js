275b903a7ec33f91e9931bd28fceff097e5825ee
eggpie76 <eggpie76@naver.com>
2026-03-13 10:55:24 +0900

    0313-02

--- web.js
+++ web.js
@@ -1,11 +1,22 @@
+const express = require("express");
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Hello, World!" }));
+var text = "express test 03: ";
+const app = express();
+
+// 바코드로 제품 정보 검색 API
+app.get("/good/:barcode", (req, res) => {
+    const barcode = req.params.barcode;
+    text += barcode
 });
 
-const PORT = process.env.PORT || 8001;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  text += ", ok";
+  res.end(JSON.stringify(text));
+});
+
+const PORT = process.env.PORT || 8001; // Cafe24에서 제공하는 포트 사용
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
